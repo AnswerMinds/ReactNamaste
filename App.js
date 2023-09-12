9d38925cd791398b5b0e0593f50deec07e3c2aa0
d3vrudra <d3vrudra@gmail.com>
2023-09-12 16:59:40 +0530

    Creating siblings inside nested structure

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 /* <div id="parent">
   <div id="child">
     <h1>I am H1 tag</h1>
+    <h2>I am H2 tag</h2>
   </div>
 </div> */
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am H1 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ])
 );
 
 console.log(parent); // heading returns a react object
