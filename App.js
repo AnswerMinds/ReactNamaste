763207bd80aaaec52cfa49d6497fd8afaccf61e5
d3vrudra <d3vrudra@gmail.com>
2023-09-12 16:49:09 +0530

    Comments on react object and render in react.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const heading = React.createElement(
   { id: "heading" },
   "Hello world from React !"
 );
-
+console.log(heading); // heading returns a react object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); // the object gets converted into Element by render
