eba4ec98f30e940be9f192c57f75812541eb21dd
answerminds <d3vrudra@gmail.com>
2023-09-15 00:00:18 +0530

    Ep2 React Parcel Prod ready app - APP ignited

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-/* <div id="parent">
-  <div id="child">
-    <h1>I am H1 tag</h1>
-    <h2>I am H2 tag</h2>
-  </div>
-</div> */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag test"),
+    React.createElement("h2", {}, "I am H2 tag test :)"),
   ])
 );
 
