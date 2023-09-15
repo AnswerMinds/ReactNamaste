333afe6a1db144f930b1f0b7f6f9732e38fdc662
answerminds <answerminds@gmail.com>
2023-09-15 17:50:58 +0530

    EP 3 React Component

--- App.js
+++ App.js
@@ -2,9 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //jsx
-const jsxHeading = <h1> Namaste React Using JSX 🚀</h1>;
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    Namaste React Using JSX 🚀
+  </h1>
+);
 
 console.log(jsxHeading);
+const TitleComponent = () => {
+  return (
+    <div>
+      <h1>Namaste React Title🚀</h1>
+    </div>
+  );
+};
+const number = 100;
+//React Functional Component
+const HeadingComponent = () => {
+  return (
+    <div>
+      <TitleComponent />
+      <TitleComponent></TitleComponent>
+      {TitleComponent()}
+      {/* component inside another component is called component composition */}
+      <h1>Namaste React Functional Component🚀</h1>
+      <h2>{number}</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading); // the object gets converted into Element by render
+root.render(<HeadingComponent />); // the object gets converted into Element by render
