efce3c3048eeb710c42b9fc3b8423b7772ff4a29
answerminds <answerminds@gmail.com>
2023-09-17 21:57:06 +0530

    React Hooks "useState"Filter feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,27 @@
 import ResList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
 const Body = () => {
+  const [listOfRestaurants, setListRestaurant] = useState(ResList.restaurants);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdRestaurants = ResList.restaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListRestaurant(filterdRestaurants);
+          }}
+        >
+          Top rated Restaurates
+        </button>
+      </div>
       <div className="res-container">
-        {ResList.restaurants.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,7 +13,7 @@ const RestaurantCard = (props) => {
         src={CDN_URL + cloudinaryImageId}
       />
       <h3>{name}</h3>
-      <h4>{cuisines.join(",")}</h4>
+      <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} Average Rating</h4>
       <h4>{sla.deliveryTime} Minutes</h4>
       <h4>{costForTwo}</h4>
