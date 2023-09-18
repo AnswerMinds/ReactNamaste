91c919e2414f84428331105a064eb33424abc02d
answerminds <answerminds@gmail.com>
2023-09-18 23:33:13 +0530

    Added Swiggy Live API & useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,25 @@
 import ResList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListRestaurant] = useState(ResList.restaurants);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const jsonData = await data.json();
+    console.log(jsonData);
+    console.log(jsonData.data.cards[1].card);
+
+    // setListRestaurant(jsonData);
+  };
   return (
     <div className="body">
       <div className="filter">
