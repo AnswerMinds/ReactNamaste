b6c5c25c32a66f60b841f6a9017e7d29eade9dbb
answerminds <answerminds@gmail.com>
2023-09-20 21:37:01 +0530

    React Routing React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
+import About from "./components/About";
 const AppLayout = () => {
   return (
     <div className="app">
@@ -11,6 +13,13 @@ const AppLayout = () => {
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <AppLayout /> },
+  { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />); // the object gets converted into Element by render
+root.render(<RouterProvider router={appRouter} />); // the object gets converted into Element by render
